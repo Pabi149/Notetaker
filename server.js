be5ed314cd9fa08bcaecd89e95eb062c94bc4d26
Pabi149 <Pabi09@icloud.com>
2022-01-30 14:47:41 -0600

    API

--- server.js
+++ server.js
@@ -1,17 +1,78 @@
-const PORT = process.env.PORT || 3001;
+// Required Dependencies
+
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
+//Initialize express app
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const allNotes = require('./db/db.json');
 
+
+//Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//GET routes
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+//POST routes
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+//Delete a note with specific id
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+//Setup Listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
